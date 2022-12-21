39ee10d5d669709d363bb165f9c60fb19dfd8330
Raylander <raylander.guimaraes@gmail.com>
2022-12-21 13:30:55 -0300

    adiciona section sobre nos

---
+++ script.js
@@ -0,0 +1,17 @@
+let myOffCanvas = document.getElementById("offcanvasNavbar");
+let offCanvasBackDrop = document.getElementsByClassName("offcanvas-backdrop")
+let body = document.getElementsByTagName("body")
+let navbar = document.getElementsByClassName("navbar")
+
+function hideMyOffCanvas() {
+    myOffCanvas.classList.remove('show');
+    offCanvasBackDrop[0].classList.remove('show')
+    body[0].style
+    console.log(body[0])
+
+   
+}
+
+
+
+// Tentar fazer o menu desaparecer ao clicar num link.
